8a428301d428e227db98341e438cf795d33973f2
raketti <raketti@mailbox.org>
2023-05-23 09:23:41 +0300

    Get data to a list and calculate average

--- ha-query.py
+++ ha-query.py
@@ -1,5 +1,6 @@
 import sqlite3
 from sqlite3 import Error
+from statistics import mean
 
 entityID = "sensor.vilp_melcloud_outside_temperature"
 metadataID = ""
@@ -30,10 +31,21 @@ def get_metadata_id_by_entity_id(conn, entityID):
     metadataID = row[0] # this will link the data from the states table
 
   cur = conn.cursor()  
-  last_row = cur.execute('SELECT * FROM statistics WHERE metadata_id=?', (metadataID,)).fetchall()[-1] # Use statistics table, as we get the mean value from there
-  temperature = last_row[4] # temperature mean value from last row, fifth column
-  print(temperature)
-    
+  temp_rows = cur.execute('SELECT * FROM statistics WHERE metadata_id=? LIMIT 100', (metadataID,)).fetchall()[-1] # Use statistics table, as we get the mean value from there
+#  temperature = last_row[4] # temperature mean value from last row, fifth column
+#  print(temperature)
+
+  temp_list = []
+  for row in temp_rows:
+    temperature = row[1] # temperature mean value from last row, fifth column
+    temp_list.append(temperature)
+#  print(temp_list)
+
+  # Calculate the average of the temperatures
+  sum_temp = sum(temp_list)
+  temp_avg = sum_temp/len(temp_list)
+  print(round(temp_avg,2))
+  
 def main():
   database = r"home-assistant_v2.db"
 
